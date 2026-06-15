9ba839410782adbb089c288f75eacbc89cf42c02
Benson Thomas <bensonthomas2024@gmail.com>
2026-06-15 14:03:35 +0930

    Initial EduRights SL Project

---
+++ translations.py
@@ -0,0 +1,39 @@
+translations = {
+
+    "English": {
+
+        "home": "Home",
+        "resources": "Resources",
+        "legal": "Legal Compliance",
+        "about": "About",
+        "contact": "Contact",
+        "dashboard": "Dashboard",
+        "login": "Login",
+        "logout": "Logout",
+        "register": "Register"
+
+    },
+
+    "Krio": {
+
+        "home": "Home",
+        "resources": "Resources",
+        "legal": "Legal Compliance",
+        "about": "About",
+        "contact": "Contact",
+        "dashboard": "Dashboard",
+        "login": "Login",
+        "logout": "Logout",
+        "register": "Register"
+
+    },
+
+    "Mende": {},
+
+    "Temne": {},
+
+    "Limba": {},
+
+    "Fullah": {}
+
+}
